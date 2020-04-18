8dc13f1e03c72b84ec6172a6ea7a53c34a160624
Amr Abdelkader <s-amr.abdelkader@zewailcity.edu.eg>
2020-04-18 22:42:03 +0200

    created lockon function for towers!

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Turret : MonoBehaviour
+{
+    public float range = 2f;
+    public string enemyTag = "Enemy";
+    public Transform part2Rotate;
+    public float turnSpeed = 10;
+    private Transform target = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+        
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        } else
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) { return; }
+       //float yDir = target.position.y - transform.position.y;
+        //Vector3 dir = new Vector3(0f, yDir);
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(part2Rotate.rotation, lookRotation, Time.deltaTime*turnSpeed).eulerAngles;
+        part2Rotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        UnityEditor.Handles.color = Color.red;
+        UnityEditor.Handles.DrawWireDisc(transform.position, transform.up, range);
+    }
+}
